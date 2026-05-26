5df2a6ea1f8523b40b2641ebc5f1fe970926d157
AndreyMih159753 <andreyka_mih@mail.ru>
2026-05-26 20:28:11 +0300

    Setted nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  css: [ `~/assets/css/main.css` ],
-  vite: { plugins: [ tailwindcss() ] }
-})
+
+  $development: {
+    devtools: { enabled: false }
+  },
+
+  $production: {
+    devtools: { enabled: false }
+  },
+
+  app: {
+    head: {
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      title: 'HyperOS Simulator'
+    }
+  },
+
+  css: [`~/assets/css/main.css`],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
+});
